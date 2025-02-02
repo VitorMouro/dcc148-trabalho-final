fb0cec6d63eef0977b2620b781085d76024ca706
Joao-Vitor-Fernandes <joviramos16@gmail.com>
2025-02-02 15:33:16 -0300

    atualizado telas de game over e creditos

---
+++ Assets/Scripts/CreditosController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditosController : MonoBehaviour
+{
+    public void MenuAgain()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
