becf21ba8bf44ccd7e6a1fc5f27caac33cb4a346
Vitor Mouro <vitor.mouro@outlook.com>
2025-03-15 16:51:43 -0300

    Movimentação

--- Trabalho Final 3D/Assets/PlayerController.cs
+++ Trabalho Final 3D/Assets/PlayerController.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float lin_speed = 1;
+    public float ang_speed = 1;
+    public GameObject trailColliderPrefab;
+    public float trailColliderInterval = 0.5f;
+    public float sensitivity = 1;
+
+    private Rigidbody rb;
+    private GameObject cam;
+    private GameObject bike;
+
+    private float inclination = 0;
+    private float lastTrailColliderTime = 0;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        cam = transform.GetChild(0).gameObject;
+        bike = transform.GetChild(1).gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 currentVelocity = rb.linearVelocity;
+        Vector3 newVelocity = transform.forward * lin_speed;
+        newVelocity.y = currentVelocity.y;
+        rb.linearVelocity = newVelocity;
+        rb.angularVelocity = Vector3.up * horizontal * ang_speed;
+
+        inclination = Mathf.Lerp(inclination, horizontal, 0.1f);
+        bike.transform.localEulerAngles = new Vector3(0, 0, -inclination * 45);
+
+        lastTrailColliderTime += Time.deltaTime;
+
+        if (lastTrailColliderTime > trailColliderInterval) {
+            lastTrailColliderTime = 0;
+            GameObject trailCollider = Instantiate(trailColliderPrefab, transform.position, transform.rotation);
+        }
+
+        UpdateCamera(Input.GetAxis("Mouse X"));
+    }
+
+    void UpdateCamera(float mouseX)
+    {
+        cam.transform.RotateAround(transform.position, Vector3.up, mouseX*sensitivity);
     }
 }
